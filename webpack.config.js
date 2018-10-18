70a35a31cf468e11354594a6dad1258f79d38cf4
semreg <vchabaniuk@outlook.com>
2018-10-19 00:51:07 +0300

    Configure webpack for es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,25 @@
 const path = require('path')
 
+module.exports = {
+  entry: './src/index.js',
 
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/dist/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: { presets: ['env'] }
+          }
+        ]
+      }
+    ]
+  }
+}
