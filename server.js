1e5d94943ced16e823b84bba3f842bab7612d04e
semreg <vchabaniuk@outlook.com>
2018-10-20 05:05:59 +0300

    Fix server.js and index.html

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000 
+
+app.use(express.static('public'))
+
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}...`)
+})
