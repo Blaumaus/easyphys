937b77fc641da3afc88c01e68295b310ce4a6434
Andrii <35810911+ookldev@users.noreply.github.com>
2018-10-20 13:42:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; 
 
-const port = process.env.PORT || 3000 
+app.use(express.static('public'));
 
-app.use(express.static('public'))
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}...`)
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('error: ', err);
+  }
+  console.log(`Listening on ${port}`);
 })
